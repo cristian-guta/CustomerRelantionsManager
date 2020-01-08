140ed28ca56e2e57652cbec2854408cd485b45d3
Cristian Guta <cristian.guta@ericsson.com>
2020-01-08 23:41:22 +0200

    My first commit

---
+++ src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,106 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.jboss.logging.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO{
+	// need to inject the session factory
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create query
+		
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by last_name", Customer.class);
+		
+		// execute query
+		
+		List<Customer> customers = theQuery.getResultList();
+		
+		// return list of customers
+		
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//save/update customer into db
+		currentSession.saveOrUpdate(theCustomer);
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		
+		//get the current hibernate session
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//retrieve/read data from db using the primary key/id passed into the method
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
+	}
+
+	@Override
+	public Object deleteCustomer(int theId) {
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+		theQuery.executeUpdate();
+		return null;
+	}
+
+	@Override
+    public List<Customer> searchCustomers(String theSearchName) {
+
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+        
+        Query<Customer> theQuery=null;
+        
+        //
+        // only search by name if theSearchName is not empty
+        //
+        if (theSearchName != null) {
+            // search for firstName or lastName ... case insensitive
+            theQuery =currentSession.createQuery("from Customer where lower(firstName) like :theSearchName or lower(lastName) like :theSearchName" , Customer.class);
+            theQuery.setParameter("theSearchName", "%" + theSearchName + "%");
+        }
+        else {
+            // theSearchName is empty ... so just get all customers
+            theQuery =currentSession.createQuery("from Customer", Customer.class);            
+        }
+        
+        // execute query and get result list
+        List<Customer> customers = theQuery.getResultList();
+        // return the results        
+        return customers;
+        
+        
+        
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,86 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"  %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>List Customers</title>
+
+<!-- reference style sheet -->
+
+<link type="text/css"
+		rel="stylesheet"
+		href="${pageContext.request.contextPath}/resources/css/style.css" />
+
+</head>
+
+<body>
+
+<div id="wrapper">
+	<div id="header">
+		<h2>CRM - Customer Relationship Manager</h2>
+	</div>
+</div>
+
+<div id="container">
+	<div id="content">
+		
+		<!-- new button : add customer -->
+		
+		<input type="button" value="Add Customer" 
+			onclick="window.location.href='showFormForAdd'; return false;"
+			class="add-button"
+		/>
+		
+		<form:form action="search" method="GET">
+                Search customer: <input type="text" name="theSearchName" />
+                
+                <input type="submit" value="Search" class="add-button" />
+        </form:form>
+		
+		<table>
+			<tr>
+				<th>First Name </th>
+				<th>Last Name </th>
+				<th>Email </th>
+				<th>Action</th>
+			</tr>
+			
+			<c:forEach var="tempCustomer" items="${customer}">
+			
+				<!-- update link with customer id -->
+				<c:url var="updateLink" value="/customer/showFormForUpdate">
+					<c:param name="customerId" value="${tempCustomer.id}" />
+					
+				</c:url>
+				
+				<!-- delete link with customer id -->
+				<c:url var="deleteLink" value="/customer/delete">
+					<c:param name="customerId" value="${tempCustomer.id}" />
+					
+				</c:url>
+				<tr>
+					<td>${tempCustomer.firstName}</td>
+					<td>${tempCustomer.lastName}</td>
+					<td>${tempCustomer.email}</td>
+					<td>
+						<a href="${updateLink}">Update</a>
+						|
+						<a href="${deleteLink}"
+							onclick="if (!(confirm('Are you sure?'))) return false">Delete</a>
+					</td>
+					
+					
+				</tr>
+			</c:forEach>
+			
+		</table>
+	</div>
+</div>
+
+</body>
+
+</html>
